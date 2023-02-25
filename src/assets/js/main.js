18eae4d2f77e3a97abac27bb002204d5d56ae1c3
han8129 <110092300+han8129@users.noreply.github.com>
2023-02-25 23:05:33 +0700

    adding fontawesone

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,8 @@
 import * as bootstrap from 'bootstrap';
 import * as jquery from 'jquery';
-import '../scss/tailwindcss.scss'
 import '../scss/main.scss'
+import '../scss/tailwindcss.scss';
+import '../../../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
 jquery(document).ready(function() {
         jquery("h1").click(function() {
