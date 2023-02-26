185b6df54650a02086267bedc21e09472e5310e7
han8129 <110092300+han8129@users.noreply.github.com>
2023-02-26 16:31:42 +0700

    Add pictures and styling to card in horizontal scroll

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,8 @@
 import * as bootstrap from 'bootstrap';
 import * as jquery from 'jquery';
-import '../scss/main.scss'
 import '../scss/tailwindcss.scss';
 import '../../../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import '../scss/main.scss'
 
 jquery(document).ready(function() {
         jquery("h1").click(function() {
