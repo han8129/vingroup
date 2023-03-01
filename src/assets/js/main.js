334e526f9501db750cb35556a38f26930387842c
han8129 <110092300+han8129@users.noreply.github.com>
2023-03-01 23:20:13 +0700

    remove tailwindcss and bootstrap import from main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,5 @@
 import * as bootstrap from 'bootstrap';
 import * as jquery from 'jquery';
-import '../scss/tailwindcss.scss';
-import '../../../node_modules/@fortawesome/fontawesome-free/css/all.css';
-import '../scss/main.scss'
 
 jquery(document).ready(function() {
         jquery("h1").click(function() {
