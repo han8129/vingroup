884786883f72b685b8dbf3eb85678e9c423c82c8
han8129 <110092300+han8129@users.noreply.github.com>
2023-03-04 23:25:49 +0700

    learn that position fixed remove the relative position

---
+++ src/assets/pictures/Bootstrap demo_files/main.js
@@ -0,0 +1,9 @@
+import './onScrollEffect.js';
+import '../../../node_modules/bootstrap/dist/js/bootstrap.js';
+import '../../../node_modules/jquery/dist/jquery.js';
+
+$(document).ready(function() {
+        $("h1").click(function() {
+                console.log("clicked ");
+        });
+});
