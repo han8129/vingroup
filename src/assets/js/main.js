7946a55539587a0014ade437a9a498cbe5f5a073
han8129 <110092300+han8129@users.noreply.github.com>
2023-03-04 15:48:01 +0700

    add onscoll effect to src/index

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,7 @@
+import './onScrollEffect.js';
+import '../../../node_modules/bootstrap/dist/js/bootstrap.js';
+import '../../../node_modules/jquery/dist/jquery.js';
+
 $(document).ready(function() {
         $("h1").click(function() {
                 console.log("clicked ");
--- src/assets/js/onScrollEffect.js
+++ src/assets/js/onScrollEffect.js
@@ -9,5 +9,5 @@ const observer = new IntersectionObserver((entries) => {
             }
     });
 });
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll('.onscroll_hidden');
 hiddenElements.forEach((element) => observer.observe(element));
