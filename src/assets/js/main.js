47f1f3f39a574a66d1e60f42461b766589acfd31
han8129 <110092300+han8129@users.noreply.github.com>
2023-03-05 16:45:39 +0700

    Add overlap at medium screen size

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,5 @@
 import './onScrollEffect.js';
 import './toggleOffcanvas.js'
-import '../../../node_modules/bootstrap/dist/js/bootstrap.js';
 import '../../../node_modules/jquery/dist/jquery.js';
 
 $(document).ready(function() {
