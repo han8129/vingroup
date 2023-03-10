c1add25781d575284ad163988cafe92f69858705
han8129 <hoang8129@gmail.com>
2023-03-11 03:51:38 +0700

    finalize the home page

--- src/assets/js/overflowScroll.js
+++ src/assets/js/overflowScroll.js
@@ -2,86 +2,121 @@
 let currentMouseX;
 let originalMouseX;
 let currentTranslateX = 0;
-const elementToTranslate = document.querySelector('.custom-scroll_overflow');
-const elementToBlur = document.querySelector('.custom_to-blur')
+let windDownWidth = window.screen.width;
 
-const cardsToAnimate = document.getElementsByClassName('custom_hover-card')
-let isDragged = false
+const elementToTranslate = document.querySelector(".custom-scroll_overflow");
+const elementToBlur = document.querySelector(".custom_to-blur");
 
+const cardsToAnimate = document.getElementsByClassName("custom_hover-card");
+let isDragged = false;
+let removeDuration500 = () => elementToTranslate.classList.remove("duration-500");
+let addDuration500 = () => elementToTranslate.classList.add("duration-500");
+
+let addBlur = () => {
+    elementToBlur.classList.add("md:blur-md");
+}
+
+let removeBlur = () => {
+    elementToBlur.classList.remove("md:blur-md");
+}
+
+function removeRedBackground() {
+    for (let index = 0; index < cardsToAnimate.length; index++) {
+        let card = cardsToAnimate[index];
+        let toDropDown = card.querySelector("span");
+
+        toDropDown.classList.add("h-0");
+        toDropDown.classList.remove("h-full");
+        toDropDown.classList.remove("p-3");
+    }
+}
+
+function resetElementTranslate() {
+    elementToTranslate.removeEventListener("pointermove", getMouseXDelta);
+    addDuration500()
+    elementToTranslate.style.removeProperty("transform");
+    removeBlur()
+    currentTranslateX = 0;
+    removeRedBackground()
+}
 
 function getMouseXDelta(event) {
-    let windDownWidth = window.screen.width
-    let isOverDragged = (
-        currentTranslateX > windDownWidth
-        || currentTranslateX < - windDownWidth
-    ) ? true : false
-
+    let rightBoundary = windDownWidth * 0.25
+    let isOverDragged = currentTranslateX > rightBoundary ? true : false
     if (isOverDragged) {
-        currentTranslateX = 0
-        originalMouseX = 0
-        elementToBlur.classList.remove("md:blur-md")
-    } else {
-        currentMouseX = event.clientX
-        currentTranslateX += currentMouseX - originalMouseX
+        resetElementTranslate()
+        return
     }
 
-    if (currentTranslateX < - 200) {
-        elementToBlur.classList.add("md:blur-md")
+    currentMouseX = event.clientX;
+    currentTranslateX += currentMouseX - originalMouseX;
+
+    if (currentTranslateX < -200) {
+        addBlur()
     } else {
-        elementToBlur.classList.remove("md:blur-md")
+        removeBlur()
     }
 
-    elementToTranslate.style.setProperty("transform", "translateX(" + currentTranslateX + "px)")
-    originalMouseX = currentMouseX
+    elementToTranslate.style.setProperty(
+        "transform",
+        "translateX(" + currentTranslateX + "px)"
+    );
+    originalMouseX = currentMouseX;
+}
+
+for (let index = 0; index < cardsToAnimate.length; index++) {
+    let card = cardsToAnimate[index];
+    let toDropDown = card.querySelector("span");
+
+    card.addEventListener("pointerover", () => {
+        toDropDown.classList.remove("h-0");
+        toDropDown.classList.add("h-full");
+        toDropDown.classList.add("p-3");
+    });
+
+    card.addEventListener("pointerleave", () => {
+        if (isDragged) return;
+        toDropDown.classList.add("h-0");
+        toDropDown.classList.remove("h-full");
+        toDropDown.classList.remove("p-3");
+    });
 }
 
 const observerScroll = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
+    entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            elementToTranslate.addEventListener('pointerdown', (pointerdown) => {
-                originalMouseX = pointerdown.clientX
+            elementToTranslate.addEventListener("pointerdown", (pointerdown) => {
+                originalMouseX = pointerdown.clientX;
+                elementToTranslate.setPointerCapture(pointerdown.pointerId);
 
-                isDragged = true
+                isDragged = true;
 
-                elementToTranslate.setPointerCapture(pointerdown.pointerId)
-                elementToTranslate.classList.remove('duration-500')
-                elementToTranslate.addEventListener('pointermove', getMouseXDelta)
+                removeDuration500()
+                elementToTranslate.addEventListener("pointermove", getMouseXDelta);
                 elementToTranslate.addEventListener(
-                    'pointerup',
-                    () => {
-                        elementToTranslate.removeEventListener('pointermove', getMouseXDelta)
-                        elementToTranslate.classList.add("duration-500")
-                        isDragged = false
+                    "pointerup",
+                    (pointerup) => {
+                        elementToTranslate.removeEventListener(
+                            "pointermove",
+                            getMouseXDelta
+                        );
+                        addDuration500()
+
+                        let pointerType = pointerup.pointerType
+                        let isTouchEvent = pointerType === 'touch' ? true : false
+
+                        if (isTouchEvent) {
+                            removeRedBackground()
+                        }
+                        isDragged = false;
                     },
                     { once: true }
-                )
-
-            })
+                );
+            });
         } else {
-            elementToTranslate.removeEventListener('pointermove', getMouseXDelta)
-            elementToTranslate.classList.add("duration-500")
-            elementToTranslate.style.removeProperty("transform")
-            elementToBlur.classList.remove("blur-md")
+            resetElementTranslate()
         }
-    })
+    });
 });
 
-observerScroll.observe(elementToTranslate)
-
-for (let index = 0; index < cardsToAnimate.length; index++) {
-    let card = cardsToAnimate[index]
-    let toDropDown = card.querySelector("span")
-
-    card.addEventListener('pointerover', () => {
-        toDropDown.classList.remove('h-0')
-        toDropDown.classList.add('h-full')
-        toDropDown.classList.add('p-3')
-    });
-
-    card.addEventListener('pointerleave', () => {
-        if (isDragged) return
-        toDropDown.classList.add('h-0')
-        toDropDown.classList.remove('h-full')
-        toDropDown.classList.remove('p-3')
-    })
-}
+observerScroll.observe(elementToTranslate);
